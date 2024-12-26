0f8461d1694e907b57b01e920e9ee571f70948c8
ganesh11t <gt20722@gmail.com>
2024-12-26 14:42:12 +0545

    HomeScreenUI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,15 +12,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // app bar
       appBar: AppBar(
         leading: Icon(CupertinoIcons.home),
         title: const Text('ChitChat'),
         actions: [
+          // Search user button
           IconButton(
             onPressed: () {},
             icon: Icon(Icons.search),
+          ),
+          // More options button
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
           )
         ],
+      ),
+
+      //floating action button to add new user
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add_comment_rounded),
+        ),
       ),
     );
   }
