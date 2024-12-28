af26093963fd625d81102a07597002bdb6370d73
ganesh11t <gt20722@gmail.com>
2024-12-28 20:36:20 +0545

    Animated Widget

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../main.dart';
+import '../home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,18 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _isAnimate = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     mq = MediaQuery.of(context).size;
@@ -24,11 +37,15 @@ class _LoginScreenState extends State<LoginScreen> {
       // body
       body: Stack(
         children: [
-          Positioned(
+          // app logo
+          AnimatedPositioned(
               top: mq.height * .15,
-              left: mq.width * .25,
+              right: _isAnimate ? mq.width * .25 : -mq.width * .5,
               width: mq.width * .5,
+              duration: const Duration(seconds: 1),
               child: Image.asset('images/icon.png')),
+
+          // login with google button
           Positioned(
               bottom: mq.height * .15,
               left: mq.width * .05,
@@ -39,16 +56,24 @@ class _LoginScreenState extends State<LoginScreen> {
                       backgroundColor: const Color.fromARGB(255, 223, 255, 187),
                       shape: StadiumBorder(),
                       elevation: 1),
-                  onPressed: () {},
+                  // Login with Google to HomeScreen
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (_) => const HomeScreen()));
+                  },
+
+                  // google icon and text
                   icon: Image.asset(
                     'images/google.png',
                     height: mq.height * .03,
                   ),
+
+                  // login with google label
                   label: RichText(
                     text: const TextSpan(
                         style: TextStyle(color: Colors.black, fontSize: 16),
                         children: [
-                          TextSpan(text: 'Sign In with '),
+                          TextSpan(text: 'Login with '),
                           TextSpan(
                               text: 'Google',
                               style: TextStyle(fontWeight: FontWeight.w500))
